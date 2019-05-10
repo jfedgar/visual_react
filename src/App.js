6aeb4e181f7b4f380654f35bf4e617ed42f94b97
jfedgar <jfedgar@gmail.com>
2019-05-10 15:30:16 -0500

    Create basic graph with react-viz

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { dsv } from 'd3-fetch';
+import moment from 'moment';
+import { XYPlot, VerticalBarSeries, XAxis, YAxis } from 'react-vis';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = { data: null };
+
+  constructor(props) {
+    super(props);
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const dataUrl = "https://data.cityofnewyork.us/api/views/25th-nujf/rows.csv";
+    const data = await dsv(",", dataUrl, (d) => {
+      return {
+        yearOfBirth: +d['Year of Birth'],
+        gender: d['Gender'],
+        ethnicity: d['Ethnicity'],
+        firstName: d['Child\'s First Name'],
+        count: +d['Count'],
+        rank: +d['Rank'],
+      };
+    });
+    this.setState({
+      rawData: data,
+      babyCountByYear: this.setBabyCountByYear(data)
+    });
+  }
+
+  setBabyCountByYear(rawData) {
+    // accumulator will be an object here (set by default at the end)
+    const entries = rawData.reduce((acc, row) => {
+      if (row.yearOfBirth in acc) {
+        // if the year is already a key in the accumulator object, then add to the count
+        acc[row.yearOfBirth] = acc[row.yearOfBirth] + row.count
+      } else {
+        // if not, create a new key for that year and set the count as value
+        acc[row.yearOfBirth] = row.count
+      }
+      return acc;
+    }, {});
+    console.log(entries);
+    return Object.entries(entries).map(([k, v]) => ({ x: +k, y: v }))
+  }
+
+  renderGraph() {
+    return (
+      <div className="App">
+        <XYPlot
+          width={600}
+          height={600}
+          margin={{
+            left: 70
+          }}
+          xType="ordinal"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <VerticalBarSeries
+            data={this.state.babyCountByYear}
+          />
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.babyCountByYear) {
+      return this.renderGraph();
+    } else {
+      return (<div></div>);
+    }
+  }
 }
 
 export default App;
